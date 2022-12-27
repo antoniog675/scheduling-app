b80955a3f93142ff8446a72d7dab72444ef85565
Antonio Gomez <antoniogomez1@hotmail.co.uk>
2022-12-27 21:03:35 +0000

    Initial commit, pushing changes to github, modified app.js and added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Component
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Profile from "./components/Profile";
+import BookUser from "./components/BookUser";
+// React toastify config
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>} /> 
+          <Route path='/register' element={<Signup/>} /> 
+          <Route path='/dashboard' element={<Dashboard/>} /> 
+          <Route path='/profile/:id' element={<Profile/>} /> 
+          <Route path='/book/:user' element={<BookUser/>} /> 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
